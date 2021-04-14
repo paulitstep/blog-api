b1d7f7f9d61301b37bcdb74d73e240e08bccda4d
paul <pasha-mo1@rambler.ru>
2021-04-14 13:28:42 +0300

    Updating Blog API

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from blog.models import Category
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class PostTests(APITestCase):
+
+    def test_view_posts(self):
+        url = reverse('blog_api:listcreate')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_postt(self):
+        self.test_category = Category.objects.create(name='django')
+        self.test_user = User.objects.create_user(username='TestUser', password='testing')
+
+        data = {"title": "new", "author": 1,
+                "excerpt": "new", "content": "new"}
+        url = reverse('blog_api:listcreate')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'blog.apps.BlogConfig',
+    'blog_api.apps.BlogApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
